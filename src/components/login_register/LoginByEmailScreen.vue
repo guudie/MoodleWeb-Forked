b8f06763f44edac175fb8e6717794f00bd72b452
hieunguyenduc696 <85797756+hieunguyenduc696@users.noreply.github.com>
2021-12-18 21:00:08 +0700

    first commit

--- src/components/login_register/LoginByEmailScreen.vue
+++ src/components/login_register/LoginByEmailScreen.vue
@@ -1,144 +1,153 @@
 <template>
-        <form @submit="onSubmit" class="email-login">
-            <div class="form-control">
-                <label class="email">Email</label>
-            </div>
-            <div class="form-control">
-                <input v-model="text" type="text" name="phoneNum" placeholder="Nhập email">
-            </div>
-            <div v-show="showEmailRequest" class="form-control" style="color:red">
-                <p>Vui lòng nhập email</p>
-            </div>
-            <div class="form-control">
-                <input v-model="verify" type="password" name="verify" placeholder="Nhập mật khẩu">
-            </div>
-            <div v-show="showPasswordRequest" class="form-control" style="color:red">
-                <p>Vui lòng nhập mật khẩu</p>
-            </div>
-            <div class="form-control">
-                <input type="submit" value="Đăng nhập" class="sign-in-btn">
-            </div>
-        </form>
+  <form @submit="onSubmit" class="email-login">
+    <div class="form-control">
+      <label class="email">Email</label>
+    </div>
+    <div class="form-control">
+      <input
+        v-model="text"
+        type="text"
+        name="phoneNum"
+        placeholder="Nhập email"
+      />
+    </div>
+    <div v-show="showEmailRequest" class="form-control" style="color:red">
+      <p>Vui lòng nhập email</p>
+    </div>
+    <div class="form-control">
+      <input
+        v-model="verify"
+        type="password"
+        name="verify"
+        placeholder="Nhập mật khẩu"
+      />
+    </div>
+    <div v-show="showPasswordRequest" class="form-control" style="color:red">
+      <p>Vui lòng nhập mật khẩu</p>
+    </div>
+    <div class="form-control">
+      <input type="submit" value="Đăng nhập" class="sign-in-btn" />
+    </div>
+  </form>
 </template>
 
 <script>
-    import { Authen } from '../../services/apis/ApiService'
-    import Button from './Button.vue'
-    export default {
-        name: 'LoginByEmail',
-        components: {
-            Button
-        },
-        data() {
-            return {
-                text: '',
-                verify: '',
-                showEmailRequest: false,
-                showPasswordRequest: false,
-            }
-        },
-        methods: {
-            onSubmit(e) {
-                e.preventDefault()
-                    if(!this.text) {
-                        this.showEmailRequest = true
-                    }
-                    if(!this.verify) {
-                        this.showPasswordRequest = true
-                    }
-                    if(!this.text || !this.verify) {
-                        return
-                    }
-                    const user = {
-                        email: this.text,
-                        password: this.verify
-                    }
-                    Authen.login(user)
-                        .then(res => this.$emit('loginToken', res))
-            },
-        },
+import { Authen } from "../../services/apis/ApiService";
+import Button from "./Button.vue";
+export default {
+  name: "LoginByEmail",
+  components: {
+    Button
+  },
+  data() {
+    return {
+      text: "",
+      verify: "",
+      showEmailRequest: false,
+      showPasswordRequest: false
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      if (!this.text) {
+        this.showEmailRequest = true;
+      }
+      if (!this.verify) {
+        this.showPasswordRequest = true;
+      }
+      if (!this.text || !this.verify) {
+        return;
+      }
+      const user = {
+        email: this.text,
+        password: this.verify
+      };
+      Authen.login(user).then(res => this.$emit("loginToken", res));
     }
+  }
+};
 </script>
 
 <style scoped>
 .container {
-    width: 640px;
-    margin: 30px auto;
-    overflow: auto;
-    min-height: 600px;
-    border: 1px solid steelblue;
-    padding: 30px;
-    border-radius: 5px;
+  width: 640px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 600px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
 }
 .email-login {
-    width: 80%;
-    align-items: center;
-    text-align: center;
-    margin: 0 auto;
+  width: 80%;
+  align-items: center;
+  text-align: center;
+  margin: 0 auto;
 }
 h1 {
-    color: #000;
-    font-weight: bold;
-    margin-bottom: 50px;
+  color: #000;
+  font-weight: bold;
+  margin-bottom: 50px;
 }
 .sign-up {
-    margin-top: 20px;
+  margin-top: 20px;
 }
 .form-control {
-    width: 100%;
-    align-items: center;
-    margin: 0 auto;
-    border: none;
-    display: flex;
+  width: 100%;
+  align-items: center;
+  margin: 0 auto;
+  border: none;
+  display: flex;
 }
 .form-control label {
-    display: block;
+  display: block;
 }
 .form-control input {
-    width: 100%;
-    height: 44px;
-    padding: 3px 7px;
-    font-size: 17px;
-    border: 2px solid #dce0e3;
-    border-radius: 25px;
-    margin-top: 10px;
+  width: 100%;
+  height: 44px;
+  padding: 3px 7px;
+  font-size: 17px;
+  border: 2px solid #dce0e3;
+  border-radius: 25px;
+  margin-top: 10px;
 }
 .form-control input:focus {
-    outline: none;
+  outline: none;
 }
 .phoneNum {
-    text-align: start;
-    flex: 1;
+  text-align: start;
+  flex: 1;
 }
 .loginWithEmail {
-    text-align: end;
-    flex: 1;
+  text-align: end;
+  flex: 1;
 }
 .send-btn {
-    padding: 9px 10px;
-    background: #dce0e3;
+  padding: 9px 10px;
+  background: #dce0e3;
 }
 .sign-in-btn {
-    margin-top: 20px;
-    letter-spacing: 2px;
-    font-weight: bold;
-    font-size: 16px;
-    color: #fff;
-    border-radius: 25px;
-    width: 100%;
-    background: rgb(29, 191, 175);
+  margin-top: 20px;
+  letter-spacing: 2px;
+  font-weight: bold;
+  font-size: 16px;
+  color: #fff;
+  border-radius: 25px;
+  width: 100%;
+  background: rgb(29, 191, 175);
 }
 .form-control p {
-    margin-bottom: 0;
+  margin-bottom: 0;
 }
 
 /* Email */
 .email {
-    text-align: start;
-    flex: 1;
+  text-align: start;
+  flex: 1;
 }
 .loginWithPhoneNum {
-    text-align: end;
-    flex: 1;
+  text-align: end;
+  flex: 1;
 }
 </style>
--- src/components/login_register/RegisterScreen.vue
+++ src/components/login_register/RegisterScreen.vue
@@ -1,86 +1,91 @@
 <template>
-    <div class="container">
-        <h1>Đăng ký tài khoản Edulanthropy</h1>
-        <RegisterByEmail @addUser="AddUser" v-show="showEmailRegister"></RegisterByEmail>
-        <router-view></router-view>
-        <div v-show="registerScreen" class="login-button">
-            <Button @btnClick="onClick" 
-            :text="'Use ' + 'email ' + '/ ' + 'phone ' + 'number '" :source="iconps" />
-            <Button :text='"Continue " + "with " + "google"' :source="icongg"/>
-            <Button :text='"Continue " + "with " + "facebook"' :source="iconfb"/>
-            <Button :text='"Continue " + "with " + "github"' :source="icongh"/>
-        </div>
-        <div class="sign-up">
-            <h3>Bạn đã có tài khoản?</h3>
-            <router-link to="/login">Đăng nhập</router-link>
-        </div>
+  <div class="container">
+    <h1>Đăng ký tài khoản Edulanthropy</h1>
+    <RegisterByEmail
+      @addUser="AddUser"
+      v-show="showEmailRegister"
+    ></RegisterByEmail>
+    <router-view></router-view>
+    <div v-show="registerScreen" class="login-button">
+      <Button
+        @btnClick="onClick"
+        :text="'Use ' + 'email ' + '/ ' + 'phone ' + 'number '"
+        :source="iconps"
+      />
+      <Button :text="'Continue ' + 'with ' + 'google'" :source="icongg" />
+      <Button :text="'Continue ' + 'with ' + 'facebook'" :source="iconfb" />
+      <Button :text="'Continue ' + 'with ' + 'github'" :source="icongh" />
     </div>
+    <div class="sign-up">
+      <h3>Bạn đã có tài khoản?</h3>
+      <router-link to="/login">Đăng nhập</router-link>
+    </div>
+  </div>
 </template>
 <script>
-    import iconperson from "../../assets/images/login_register/personal.svg";
-    import icongoogle from "../../assets/images/login_register/google.svg";
-    import iconfacebook from "../../assets/images/login_register/facebook.svg";
-    import icongithub from "../../assets/images/login_register/github.svg";
-    import { Authen } from '../../services/apis/ApiService'
-    import Button from './Button.vue'
-    import RegisterByEmail from './RegisterByEmailScreen.vue'
-    export default {
-        name: 'RegisterScreen',
-        components: {
-            Button,
-            RegisterByEmail
-        },
-        methods: {
-            onClick() {
-                this.showEmailRegister = true
-            },
-            AddUser(newUser) {
-                Authen.register(newUser)
-                    .then(res => console.log(res))
-                    .catch(e => console.log(e))
-            }
-        },
-        data() {
-            return {
-                showEmailRegister: false,
-                iconps: iconperson,
-                icongg: icongoogle,
-                iconfb: iconfacebook,
-                icongh: icongithub,
-            }
-        },
-        computed: {
-            registerScreen() {
-                if(this.showEmailRegister === false) {
-                    return true
-                }
-                return false
-            }
-        },
+import iconperson from "../../assets/images/login_register/personal.svg";
+import icongoogle from "../../assets/images/login_register/google.svg";
+import iconfacebook from "../../assets/images/login_register/facebook.svg";
+import icongithub from "../../assets/images/login_register/github.svg";
+import { Authen } from "../../services/apis/ApiService";
+import Button from "./Button.vue";
+import RegisterByEmail from "./RegisterByEmailScreen.vue";
+export default {
+  name: "RegisterScreen",
+  components: {
+    Button,
+    RegisterByEmail
+  },
+  methods: {
+    onClick() {
+      this.showEmailRegister = true;
+    },
+    AddUser(newUser) {
+      Authen.register(newUser)
+        .then(res => console.log(res))
+        .catch(e => console.log(e));
     }
+  },
+  data() {
+    return {
+      showEmailRegister: false,
+      iconps: iconperson,
+      icongg: icongoogle,
+      iconfb: iconfacebook,
+      icongh: icongithub
+    };
+  },
+  computed: {
+    registerScreen() {
+      if (this.showEmailRegister === false) {
+        return true;
+      }
+      return false;
+    }
+  }
+};
 </script>
 
 <style scoped>
 .container {
-    width: 640px;
-    margin: 30px auto;
-    overflow: auto;
-    min-height: 600px;
-    border: 1px solid steelblue;
-    padding: 30px;
-    border-radius: 5px;
-    
-  }
+  width: 640px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 600px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
 .login-button {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 h1 {
-    color: #000;
-    font-weight: bold;
-    margin-bottom: 50px;
+  color: #000;
+  font-weight: bold;
+  margin-bottom: 50px;
 }
 .sign-up {
-    margin-top: 20px;
+  margin-top: 20px;
 }
 </style>
